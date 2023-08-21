c17c31e5303b0610ae7b6d0d0d880247335306a0
Dave <d.ruocco1987@googlemail.com>
2023-08-21 15:06:19 +0100

    Adds backend logic and endpoint for /info

--- index.js
+++ index.js
@@ -33,6 +33,15 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  let numberOfPeople = 0;
+  const date = new Date();
+  persons.forEach(() => numberOfPeople++);
+  res.send(
+    `<p>Phonebook has info for ${numberOfPeople} people.</p><br /><p>${date}</p>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
